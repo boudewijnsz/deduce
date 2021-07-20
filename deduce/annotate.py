42d2d1f44f02a83ed515e1728f9b9c061a3caf53
boudewijnsz <72131937+boudewijnsz@users.noreply.github.com>
2021-07-20 15:25:41 +0200

    regex telefoonnummer aangepast

--- deduce/annotate.py
+++ deduce/annotate.py
@@ -442,7 +442,7 @@ def annotate_phonenumber(text):
                   "<TELEFOONNUMMER \\1>",
                   text)
 
-    text = re.sub("((\(\d{3}\)|\d{3})\s?\d{3}\s?\d{2}\s?\d{2})(?![^<]*>)",
+    text = re.sub("((?<=\s)|(?<=^))\(?0\d\d\)?[\d ]*",
                   "<TELEFOONNUMMER \\1>",
                   text)
 
